4b8876a97b2f98f23123dad644b143643ef87696
Darren Tebo <darrentebo83@outlook.com>
2020-10-02 11:51:59 -0400

    wrote the required tests for the checkout form

--- client/src/components/CheckoutForm.test.js
+++ client/src/components/CheckoutForm.test.js
@@ -1,9 +1,26 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { fireEvent, render, screen, waitFor } from "@testing-library/react";
+import { act } from "react-dom/test-utils";
 import CheckoutForm from "./CheckoutForm";
 
 // Write up the two tests here and make sure they are testing what the title shows
 
-test("form header renders", () => {});
+test("form header renders", () => {
+    render(<CheckoutForm />);
 
-test("form shows success message on submit with form details", () => {});
+    const header = screen.findByText(/Checkout Form/i);
+
+    expect(header).toBeTruthy();
+});
+
+test("form shows success message on submit with form details", async () => {
+    render(<CheckoutForm />);
+
+    const button = screen.getByRole("button", {name: /checkout/i});
+
+    fireEvent.click(button);
+
+    const successMessage = await screen.findByTestId(/successMessage/i);
+
+    expect(successMessage).toBeInTheDocument();
+});
